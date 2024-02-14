7c8f1bfc928d95771db7c4d755dd123c7506a5b0
Diego Sanchez Briales <105086398+Diegobrls@users.noreply.github.com>
2024-02-14 09:57:04 +0100

    depuración de código en beforeinstall.sh

--- codedeploy-scripts/beforeinstall.sh
+++ codedeploy-scripts/beforeinstall.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+echo "redirección a carpeta"
 cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
 
+echo "dando permisos"
 #Modifico los permisos del gradlew
 chmod +x gradlew
 
+echo "ejecutando gradlew"
 ./gradlew war
